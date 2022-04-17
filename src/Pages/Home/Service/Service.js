20d719dea00135f8193081afdd67825bcbe63d12
Nayim hossen <nayimh2015@gmail.com>
2022-04-17 21:14:14 +0600

    services section desing and responsive

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Service = ({ service }) => {
+
+    const { name, img, description, price } = service;
+
+    return (
+        <div className='border-2 p-6 rounded text-center'>
+            <img src={img} alt="" />
+            <h2>This is service: {name}</h2>
+            <p>Price: {price}</p>
+            <p><small>{description}</small></p>
+            <button className='bg-red-600 p-2 rounded text-white'>Book: {name}</button>
+        </div>
+    );
+};
+
+export default Service;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+
     return (
-        <div>
-            <h2>This is Services</h2>
+        <div className='container'>
+            <h2 className='text-center text-4xl font-bold'>My Services</h2>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    >
+                    </Service>)
+                }
+            </div>
         </div>
     );
 };
